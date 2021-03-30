489811317497efb42f520c807c51fb92d5b2365c
soch-ctrl <chadt7oficial@gmail.com>
2021-03-30 13:10:39 -0300

    Variaveis

---
+++ quinzena3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,119 @@
+
+//EXERCÍCIO DE INTEREPRETAÇÃO DE CÓDIGO
+
+/*1-
+console.log(b) imprimirá 10
+console.log(a,b) imprimirá 
+10, 5*/
+
+//EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+//2- console.log(a, b, c) imprimirá (10, 10, 10)
+
+//1a const nome
+
+//1b const idade
+
+//1c console.log(typeof nome, idade
+
+//1d Recebi o tipo undefined, o que significa que não há nenhum valor dentro das variáveis
+
+/*1d
+nome = prompt("Qual seu nome?")
+
+idade = prompt("Qual sua idade?")
+
+console.log(nome, idade)*/
+
+/*1e
+nome = prompt("Qual seu nome?")
+
+idade = prompt("Qual sua idade?")
+
+console.log(nome, idade)
+
+console.log(typeof nome, idade)*/
+
+/*1f
+nome = prompt("Qual seu nome?")
+
+idade = prompt("Qual sua idade?")
+
+console.log(nome, idade)
+
+alert('Olá ' + nome + ', você tem ' + idade +' anos.')*/
+
+/* 2
+let endereco
+let corFavorita
+let serieFavorita
+let inspiracao
+let animalDeEstimacao
+
+endereco = prompt("Qual seu endereço")
+
+console.log(endereco)
+
+corFavorita = prompt("Qual sua cor favorita?")
+
+console.log(corFavorita)
+
+serieFavorita = prompt("Qual sua série favorita?")
+
+console.log(serieFavorita)
+
+inspiracao = prompt("Cite uma pessoa que te inspira")
+
+console.log(inspiracao)
+
+animalDeEstimacao = prompt("Tem animais de estimação?")
+
+console.log(animalDeEstimacao)*/
+
+/* 3a
+comidasFavoritas = ["Pizza","Macarrão","Lanche","Berinjela","Champignon"]
+
+console.log(comidasFavoritas)*/
+
+/*3b
+comidasFavoritas = ["Pizza","Macarrão","Lanche","Berinjela","Champignon"]
+
+console.log("Essas são as minhas comidas favoritas")
+
+console.log(comidasFavoritas[0])
+console.log(comidasFavoritas[1])
+console.log(comidasFavoritas[2])
+console.log(comidasFavoritas[3])
+console.log(comidasFavoritas[4])*/
+
+/*3c
+ comidasFavoritas = ["Pizza","Macarrão","Lanche","Berinjela","Champignon"]
+
+comidasFavoritas[1] = prompt("Qual sua comida favorita?")
+
+console.log(comidasFavoritas)
+*/
+
+/*4a
+perguntas = ["Você estuda?", "Gosta de refrigerantes?", "Possui veículo?"]
+
+respostas = [true, false, true]*/
+
+/*4b
+console.log(perguntas[0], respostas[0])
+
+
+console.log(perguntas[1], respostas[1])
+
+
+console.log(perguntas[2], respostas[2])*/
+
+
+
+
+
+
+
+
+
+
