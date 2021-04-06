e9a3c24b232f7d6fe63939752fb6fc4c57cbcbca
soch-ctrl <chadt7oficial@gmail.com>
2021-04-06 20:26:57 -0300

    Condicionais com Desafio1

---
+++ quinzena3/condicionais/template-aula12/src/exemplos/index.js
@@ -0,0 +1,81 @@
+/* Exercício 1
+O código pede que o usuário digite um número, se ele for um número par, ele é aprovado, e caso seja ímpar ele é reprovado.*/
+
+/* Exercício 2
+ a) Informar o preço das frutas no mercado. 
+ b) O preço da fruta Maça é R$2.25
+ c) O preço da fruta Pêra é R$5
+ */
+
+ /* Exercício 3
+ a) Armazenar a resposta do usuário na na variável numero
+ b) "Esse número passou no teste", caso fosse -10 não seria imprimido nada no console.
+ c) A variável mensagem não vai ser impressa pois seu console.log está fora do bloco if.
+ */ 
+
+ /*Exercício 4
+let idadeUsuario = prompt("Qual sua idade?")
+let idade = Number(idadeUsuario)
+ 
+
+if(idade >= 18){
+    console.log("Você pode dirigir :)")
+} else {
+     console.log("Você não pode dirigir :(")
+} */
+
+/*Exercício 5
+let turnoAluno = prompt("Qual turno você estuda? M para matutino, V para vespertino e N para noturno")
+
+if(turnoAluno === 'M'){
+    console.log("Bom Dia!!")
+} else if(turnoAluno === 'N'){
+    console.log("Boa Noite!!")
+} else {
+    console.log("Boa Tarde!!")
+}*/
+
+/*Exercício 6
+let turnoAluno = prompt("Qual turno você estuda?   M para matutino, V para vespertino e N para noturno")
+
+switch(turnoAluno){
+    case 'M':
+        console.log("Bom Dia!!")
+        break
+     case 'V':
+         console.log("Boa Tarde!!")
+         break
+     case 'N':
+         console.log("Boa Noite!!") 
+         break      
+}*/
+
+/*Exercício 7
+ let generoFilme = prompt("Qual o gênero do filme?")
+
+ let preco = prompt("Qual preço do ingresso ?")
+
+if(generoFilme === 'Fantasia' || preco >= 15){
+    console.log("Bom Filme!!")
+} else {
+    console.log("Escolha outro filme :( ")
+}
+*/
+
+/* DESAFIO 1
+let generoFilme = prompt("Qual o gênero do filme?")
+
+let preco = prompt("Qual preço do ingresso ?")
+
+let snack = prompt("Qual snack você quer comprar?")
+
+if(generoFilme === 'Fantasia' || preco >= 15){
+  console.log("Bom filme!", "com", snack, " =D")
+} else {
+   console.log("Escolha outro filme :( ")
+}*/
+
+
+
+
+    
