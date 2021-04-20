73c57d5d01e138d1569b5b67c0aed91ae8f347fe
soch-ctrl <chadt7oficial@gmail.com>
2021-04-20 18:19:17 -0300

    Blog Incompleto

---
+++ quinzena4/objetos/blog/index.js
@@ -0,0 +1,19 @@
+
+
+const post = {
+    titulo: document.getElementById("titulo-post").value,
+
+    autor: document.getElementById("autor-post").value,
+
+    conteudo: document.getElementById("conteudo-post").value
+
+
+}
+
+const postBlog = [];
+
+postBlog.push(post)
+
+console.log(post)
+
+
